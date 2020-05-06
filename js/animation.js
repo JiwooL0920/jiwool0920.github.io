5904118833cac16084dbd466c8946d549fcdcf20
JiwooL0920 <JiwooL0920@gmail.com>
2020-05-06 12:50:35 -0400

    push

---
+++ js/animation.js
@@ -0,0 +1,35 @@
+const typedTextSpan = document.querySelector(".typed-text");
+
+const textArray = ["Description1","Description2","Description3"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+        //type
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    } else {
+        //erase
+        setTimeout(erase,newTextDelay);
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0,charIndex-1);
+        charIndex--;
+        setTimeout(erase,erasingDelay);
+    } else {
+        //type
+        textArrayIndex++;
+        if (textArrayIndex >= textArray.length) textArrayIndex=0;
+        setTimeout(type,typingDelay + 1100);
+    }
+}
+
+type();
